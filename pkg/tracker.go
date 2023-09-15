f7442538202db874b0ca9b05a2f6ffcedd0b1887
Antoine Boucher <antoine.bou13@gmail.com>
2023-09-15 16:10:25 -0400

    Update tracker.go

--- pkg/tracker.go
+++ pkg/tracker.go
@@ -1,1 +1,30 @@
+package tracker
 
+import (
+	"github.com/caddyserver/caddy/v2"
+	"github.com/caddyserver/caddy/v2/modules/caddyhttp"
+	"net/http"
+)
+
+func init() {
+	caddy.RegisterModule(Tracker{})
+}
+
+type Tracker struct {
+	// Your configuration fields here
+}
+
+// Caddy module name
+func (Tracker) CaddyModule() caddy.ModuleInfo {
+	return caddy.ModuleInfo{
+		ID:  "http.handlers.tracker",
+		New: func() caddy.Module { return new(Tracker) },
+	}
+}
+
+func (m *Tracker) ServeHTTP(w http.ResponseWriter, r *http.Request, next caddyhttp.Handler) error {
+	// Your logic to track users, possibly using cookies or other methods
+	
+	// Call the next handler in the chain
+	return next.ServeHTTP(w, r)
+}
