9c0f0b4448d8cbc7fd1d6d1d3f8fe995af176e15
Antoine Boucher <antoine.bou13@gmail.com>
2023-09-15 16:19:05 -0400

    Update tracker.go

--- pkg/tracker.go
+++ pkg/tracker.go
@@ -1,30 +1,73 @@
-package tracker
+package main
 
 import (
-	"github.com/caddyserver/caddy/v2"
-	"github.com/caddyserver/caddy/v2/modules/caddyhttp"
-	"net/http"
+    "net/http"
+    "github.com/caddyserver/caddy/v2"
+    "github.com/caddyserver/caddy/v2/modules/caddyhttp"
+    "github.com/prometheus/client_golang/prometheus"
+    "github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+// Define custom metrics
+var (
+    requestMethod = prometheus.NewCounterVec(
+        prometheus.CounterOpts{
+            Name: "http_requests_method_total",
+            Help: "Total number of requests by HTTP method.",
+        },
+        []string{"method"},
+    )
+    requestURI = prometheus.NewCounterVec(
+        prometheus.CounterOpts{
+            Name: "http_requests_uri_total",
+            Help: "Total number of requests by URI.",
+        },
+        []string{"uri"},
+    )
+    userAgent = prometheus.NewCounterVec(
+        prometheus.CounterOpts{
+            Name: "http_requests_user_agent_total",
+            Help: "Total number of requests by User-Agent.",
+        },
+        []string{"user_agent"},
+    )
+    remoteIP = prometheus.NewCounterVec(
+        prometheus.CounterOpts{
+            Name: "http_requests_remote_ip_total",
+            Help: "Total number of requests by remote IP.",
+        },
+        []string{"remote_ip"},
+    )
 )
 
 func init() {
-	caddy.RegisterModule(Tracker{})
+    // Register the custom metrics with Prometheus
+    prometheus.MustRegister(requestMethod, requestURI, userAgent, remoteIP)
+
+    // Register the module with Caddy
+    caddy.RegisterModule(PrometheusGeoIP{})
 }
 
-type Tracker struct {
-	// Your configuration fields here
+type PrometheusGeoIP struct{}
+
+func (PrometheusGeoIP) CaddyModule() caddy.ModuleInfo {
+    return caddy.ModuleInfo{
+        ID:  "http.handlers.prometheus_geoip",
+        New: func() caddy.Module { return new(PrometheusGeoIP) },
+    }
 }
 
-// Caddy module name
-func (Tracker) CaddyModule() caddy.ModuleInfo {
-	return caddy.ModuleInfo{
-		ID:  "http.handlers.tracker",
-		New: func() caddy.Module { return new(Tracker) },
-	}
-}
+// ServeHTTP implements the Caddy HTTP handler interface
+func (m *PrometheusGeoIP) ServeHTTP(w http.ResponseWriter, r *http.Request, next caddyhttp.Handler) error {
+    // Increment the custom metrics based on the incoming request
+    requestMethod.WithLabelValues(r.Method).Inc()
+    requestURI.WithLabelValues(r.RequestURI).Inc()
+    userAgent.WithLabelValues(r.Header.Get("User-Agent")).Inc()
+    remoteIP.WithLabelValues(r.RemoteAddr).Inc()
 
-func (m *Tracker) ServeHTTP(w http.ResponseWriter, r *http.Request, next caddyhttp.Handler) error {
-	// Your logic to track users, possibly using cookies or other methods
-	
-	// Call the next handler in the chain
-	return next.ServeHTTP(w, r)
+    // Serve Prometheus metrics
+    promhttp.Handler().ServeHTTP(w, r)
+
+    // Call the next handler in the chain
+    return next.ServeHTTP(w, r)
 }
