df76196630b84e7635d6e6b63723c8ee97065028
Antoine Boucher <antoine.bou13@gmail.com>
2023-09-15 16:10:39 -0400

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,10 @@
+package main
 
+import (
+	"github.com/caddyserver/caddy/v2/caddyconfig/caddyfile"
+	_ "github.com/yourusername/caddy-tracker/pkg/tracker"
+)
+
+func main() {
+	// You can add some local testing code here or leave it empty
+}
